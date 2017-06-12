02df167feae2ff73a59c45fba8bfc6ca7f3dcaf5
fanybook <4r3e2w!Q>
2017-06-12 20:44:52 +0800

    add some middleware config

--- src/Config/base_middlewares.php
+++ src/Config/base_middlewares.php
@@ -2,18 +2,27 @@
 
 return [
     'master_middlewares' => [
-        'scheduler_middleware_01' => 1,
-        'scheduler_middleware_02' => 2,
-        'scheduler_middleware_03' => 3,
-        'scheduler_middleware_04' => 4,
-        'scheduler_middleware_05' => 5,
+        'HttpErrorMiddleware'       => 50,      // http错误
+        'OffsiteMiddleware'         => 500,     // 该中间件过滤出所有主机名不在job属性 allowed_domains 的request
+        'RefererMiddleware'         => 700,     // 生成，Request的referer字段
+        'UrlLengthMiddleware'       => 800,     // url长度
+        'DepthMiddleware'           => 900,     // 爬行深度
     ],
 
     'worker_middlewares' => [
-        'downloader_middleware_01' => 1,
-        'downloader_middleware_02' => 2,
-        'downloader_middleware_03' => 3,
-        'downloader_middleware_04' => 4,
-        'downloader_middleware_05' => 5,
+        'RobotsTxtMiddleware'       => 100,     // robots.txt
+        'HttpAuthMiddleware'        => 300,     // auth的应对
+        'DownloadTimeoutMiddleware' => 350,     // 下载超时处理
+        'DefaultHeadersMiddleware'  => 400,     // 默认的请求头
+        'UserAgentMiddleware'       => 500,     // ua模拟
+        'RetryMiddleware'           => 550,     // 重试处理，处理优先级放入堆栈
+        'AjaxCrawlMiddleware'       => 560,     // Ajax的爬行
+        'MetaRefreshMiddleware'     => 580,     // meta刷新的应对
+        'HttpCompressionMiddleware' => 590,     // http压缩的应对
+        'RedirectMiddleware'        => 600,     // 301的应对
+        'CookiesMiddleware'         => 700,     // cookies的处理
+        'HttpProxyMiddleware'       => 750,     // 代理的使用
+        'DownloaderStats'           => 850,     // 下载状态
+        'HttpCacheMiddleware'       => 900,     // http缓存
     ],
 ];
--- src/Master/Master.php
+++ src/Master/Master.php
@@ -4,11 +4,101 @@ namespace Antman\Master;
 
 class Master
 {
-    public function start()
+    protected $job_space = [];
+
+    // 这些中间件是负责处理爬行前调度的
+
+    public function __construct()
     {
-        $this
-            ->setName('dig {job}')
-            ->setDescription('Let ant execute a dig job.')
-            ->setHelp('find/create a job in \'src/Jobs\'');
+        $this->job_space = [
+            'request_queue' => [],
+            'request_queue_lock' => false,
+            'scheduler_temp' => [],
+            'scheduler_trace' => [],
+        ];
+
+        $this->base_config = reqeire('../Config/base_middlewares.php');
+    }
+
+    private function _validate()
+    {
+        // nothing
+    }
+
+    private function _mergeMiddleware()
+    {
+        if (!empty($this->job->config['master_middlewares'])) {
+            $this->job->config['master_middlewares'] = array_merge(
+                $this->base_config['master_middlewares'],
+                $this->job->config['master_middlewares']
+            );
+            asort($this->job->config['master_middlewares']);
+        }
+
+        if (!empty($this->job->config['worker_middlewares'])) {
+            $this->job->config['worker_middlewares'] = array_merge(
+                $this->base_config['worker_middlewares'],
+                $this->job->config['worker_middlewares']
+            );
+            asort($this->job->config['worker_middlewares']);
+        }
+    }
+
+    private function _processStartUrls()
+    {
+        $new_urls = [];
+        foreach ($this->job->start_urls as $idx => $item) {
+            if (is_numeric($idx)) {
+                $new_urls[] = $item;
+                echo $item,"\n";
+            } else {
+                list($min, $max) = explode('-', $idx);
+                for ($i = $min; $i <= $max; $i++) {
+                    $new_urls[] = preg_replace('/({\$\w+})/', $i, $item);
+                    echo preg_replace('/({\$\w+})/', $i, $item),"\n";
+                }
+            }
+        }
+
+        $this->job->start_urls = $new_urls;
+    }
+
+    private function _createRequest()
+    {
+        // nothing
+    }
+
+    public function start($job = null)
+    {
+        if (!$job) {
+            die('爬行作业为空，请检查后再试！');
+        }
+
+        $this->job = $job;
+        $this->_validate();
+        $this->_mergeMiddleware();
+        $this->_processStartUrls();
+        $this->_createRequest();
+
+//        var_dump($this->job->start_urls);
+
+        // 如果 config 里设置了延迟，那么全部变成单线程
+        // 否则变成多线程，一次10个，可以配置
+
+        // $request = new Request();
+        // $request->url = $this->job->url; // 多个循环，或通过规则生成多个入口url
+        // Scheduler($request, $this->job);
+
+    }
+
+    public function push($request)
+    {
+        // push 给 Scheduler
+    }
+
+    public function stop($job_name = null)
+    {
+        // 去redis里，找到任务的url栈，或者改一下任务状态
+        // 让采集热停止下来，把url栈备份一下
     }
 }
